cbefee8e8959398d4845095dc9c678f473fa0b0b
Elder <elder_louzada@outlook.com>
2019-10-22 19:26:00 -0300

    atualiza ex02 do Cap 9

--- Cap 9/ex02.hs
+++ Cap 9/ex02.hs
@@ -2,6 +2,6 @@ module Main where
 
 main :: IO ()
 main = do
-  putStrLn "Digite uma palavra: "
-  palavra <- getLine
-  putStrLn $ "Palavra invertida: " ++ reverse palavra
+  putStrLn "Digite uma palavra"
+  x <- getLine
+  putStrLn $ "A palavra ao contrário é: "++ reverse x
